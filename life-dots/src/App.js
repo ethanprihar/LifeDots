a0b2eeeec286401e759e45321d8d86a66d2e538e
Ethan Prihar <ethanprihar@gmail.com>
2020-10-10 22:42:59 -0400

    finished genome class

--- life-dots/src/App.js
+++ life-dots/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Genome from './models/Genome';
 
-import Genome from './models/Genome.js';
-import Dot from './models/Dot.js';
+//import Genome from './models/Genome.js';
+//import Dot from './models/Dot.js';
 
-const math = require('mathjs')
+var math = require("mathjs");
+var ndarray = require("ndarray");
+var ops = require("ndarray-ops");
 
 function App() {
-  let sexNum = 2.4;
-  let sexMin = -10.3;
-  let sexMax = 100.7;
-  let sexOdds = [[1.3, 1.2], [3.1, 1.1]];
-  let maxSize = [132.3, 13.2];
-  let sexSize = [0.8, 0.86];
-  let babySize = [0.8, 0.53];
-  let signalNum = [1.2, 3.1];
-  let eatOdds = [[3.1, 1.4], [1.1, 1.1]];
-  let speed = [3.2, 2.2];
-  let view = [3.1, 0.6];
-  let moveWeights = [math.zeros(Math.pow(Math.floor(view[0]) * 2 + 1, 2), Math.floor(signalNum[0]) + 9), math.zeros(Math.pow(Math.floor(view[1]) * 2 + 1, 2), Math.floor(signalNum[1]) + 9)];
-  let g = new Genome(sexNum, sexMin, sexMax, sexOdds, maxSize, sexSize, babySize, signalNum, eatOdds, speed, view, moveWeights);
-  let new_g = g.mutate(0.01);
-  let d = new Dot(true, new_g)
+  console.log('Genome Test')
+  let ally_min = -10.6;
+  let ally_max = 100.4;
+  let max_size = 200.2;
+  let baby_frac = 0.8;
+  let eat_ratio = 0.5;
+  let speed = 4.2;
+  let weights = ndarray(new Float64Array(450), [45,10]);
+  let g1 = new Genome(ally_min, ally_max, max_size, baby_frac, eat_ratio, speed, weights);
+  let g2 = g1.mutate(0.1);
+  console.log(g1);
+  console.log(g2);
   return (
     <div className="App">
       <header className="App-header">
--- life-dots/src/models/Genome.js
+++ life-dots/src/models/Genome.js
@@ -1,211 +1,57 @@
-const math = require('mathjs');
+var ndarray = require("ndarray");
+var ops = require("ndarray-ops");
 
 export default class Genome
 {
-    constructor(sexNum, sexMin, sexMax, sexOdds, 
-                maxSize, sexSize, babySize, signalNum, 
-                eatOdds, speed, view, moveWeights)
+    constructor(ally_min, ally_max, max_size, baby_frac, eat_ratio, speed, weights)
     {
-        // The number of sexes of this dot's species
-        this.sexNum = sexNum;
-        // The min value to compare to other dot's when determining their chance to successfully reproduce
-        this.sexMin = sexMin
-        // The max value to compare to other dot's when determining their chance to successfully reproduce
-        this.sexMax = sexMax
-
-        // All the following variables are arrays with one element for each sex
-        // The probability distribution for which sex an offspring will be
-        this.sexOdds = sexOdds;
-        // The maximum size a dot can become, past this it's size will not grow
-        this.maxSize = maxSize;
-        // The fraction of a dot's max size it must be before being able to reproduce
-        this.sexSize = sexSize;
-        // The fraction of a dot's sex size that will be taken from the dot and put into its offspring 
-        this.babySize = babySize;
-        // The number of signals a dot has
-        this.signalNum = signalNum;
-        // The food to dot odds of size trasfered to dot size
-        this.eatOdds = eatOdds;
+        // The minimum value of the range which determines which dots are allies.
+        this.ally_min = ally_min;
+        // The maximum value of the range which determines which dots are allies.
+        this.ally_max = ally_max;
+        // The maximum size a dot can become, at which point it will split
+        this.max_size = max_size;
+        // The fraction of a dot's max size it will split off of itself
+        this.baby_frac = baby_frac;
+        // The odds of food consumption to dot consumption
+        this.eat_ratio = eat_ratio;
         // The number of ticks between dot moves
         this.speed = speed;
-        // The square radius around the dot that the dot perceives
-        this.view = view;
         // The weight matrix used of the multi-regression
-        this.moveWeights = moveWeights;
+        this.weights = weights;
     }
 
-    mutate(maxMutPct)
+    mutate(max_mut_pct)
     {
-        let newSexNum;
-        let newSexMin;
-        let newSexMax;
-        let newSexOdds;
-        let newMaxSize;
-        let newSexSize;
-        let newBabySize;
-        let newSignalNum;
-        let newEatOdds;
-        let newSpeed;
-        let newView;
-        let newMoveWeights;
-
-        // Mutate sexNum
-        newSexNum = this.geneMut(this.sexNum, maxMutPct);
-        // Check if newSexNum violates constraints
-        if (Math.floor(newSexNum) == 0){return null;}
-        // Adjust other values based on newSexNum
-        else if (Math.floor(newSexNum) < Math.floor(this.sexNum))
-        {
-            newSexOdds = [];
-            for (let odds of this.sexOdds.slice(0, -1))
-            {
-                newSexOdds.push(odds.slice());
-            }
-            newMaxSize = this.maxSize.slice(0, -1);
-            newSexSize = this.sexSize.slice(0, -1);
-            newBabySize = this.babySize.slice(0, -1);
-            newEatOdds = [];
-            for (let odds of this.eatOdds.slice(0, -1))
-            {
-                newEatOdds.push(odds.slice());
-            }
-            newSpeed = this.speed.slice(0, -1);
-            newSignalNum = this.signalNum.slice(0, -1);
-            newView = this.view.slice(0, -1);
-            newMoveWeights = [];
-            for (let weights of this.moveWeights.slice(0, -1))
-            {
-                newMoveWeights.push(math.clone(weights));
-            }
-        }
-        else if (Math.floor(newSexNum) > Math.floor(this.sexNum))
-        {
-            let i = this.sexNum - 1;
-            newSexOdds = [];
-            for (let odds of this.sexOdds)
-            {
-                newSexOdds.push(odds.slice());
-            }
-            newSexOdds.push(this.sexOdds[i].slice());
-            newMaxSize = this.maxSize.slice().push(this.maxSize[i]);
-            newSexSize = this.sexSize.slice().push(this.sexSize[i]);
-            newBabySize = this.babySize.slice().push(this.babySize[i]);
-            newEatOdds = [];
-            for (let odds of this.eatOdds)
-            {
-                newEatOdds.push(odds.slice());
-            }
-            newEatOdds.push(this.eatOdds[i].slice());
-            newSpeed = this.speed.slice().push(this.speed[i]);
-            newSignalNum = this.signalNum.slice().push(this.signalNum[i]);
-            newView = this.view.slice().push(this.view[i]);
-            newMoveWeights = [];
-            for (let weights of this.moveWeights)
-            {
-                newMoveWeights.push(math.clone(weights));
-            }
-            newMoveWeights.push(math.clone(this.moveWeights[i]))
-        }
-        else
-        {
-            newSexOdds = [];
-            for (let odds of this.sexOdds)
-            {
-                newSexOdds.push(odds.slice());
-            }
-            newMaxSize = this.maxSize.slice();
-            newSexSize = this.sexSize.slice();
-            newBabySize = this.babySize.slice();
-            newEatOdds = [];
-            for (let odds of this.eatOdds)
-            {
-                newEatOdds.push(odds.slice());
-            }
-            newSpeed = this.speed.slice();
-            newSignalNum = this.signalNum.slice();
-            newView = this.view.slice();
-            newMoveWeights = [];
-            for (let weights of this.moveWeights)
-            {
-                newMoveWeights.push(math.clone(weights));
-            }
-        }
-
-        // Mutate sexMin
-        newSexMin = this.geneMut(this.sexMin, maxMutPct);
-
-        // Mutate sexMax
-        newSexMax = this.geneMut(this.sexMax, maxMutPct);
-
-        for (let i = 0; i < Math.floor(newSexNum); i++)
-        {
-            // Mutate sexOdds
-            for (let o = 0; o < newSexOdds[i].length; o++)
-            {
-                newSexOdds[i][o] = this.geneMut(newSexOdds[i][o], maxMutPct);
-                // Check if newSexOdds violates constrains
-                if (newSexOdds[i][o] <= 0){return null;}
-            }
-
-            // Mutate maxSize
-            newMaxSize[i] = this.geneMut(newMaxSize[i], maxMutPct);
-            // Check if newMaxSize violates constraints
-            if (newMaxSize[i] <= 0){return null;}
-
-            // Mutate sexSize
-            newSexSize[i] = this.geneMut(newSexSize[i], maxMutPct);
-            // Check if newSexSize violates constraints
-            if (newSexSize[i] <= 0 || newSexSize[i] >= 1){return null;}
-
-            // Mutate babySize
-            newBabySize[i] = this.geneMut(newBabySize[i], maxMutPct);
-            // Check if newBabySize violates constraints
-            if (newBabySize[i] <= 0 || newBabySize[i] >= 1){return null;}
-
-            // Mutate eatOdds
-            for (let o = 0; o < newEatOdds[i].length; o++)
-            {
-                newEatOdds[i][o] = this.geneMut(newEatOdds[i][o], maxMutPct);
-                // Check if newEatOdds violates constrains
-                if (newEatOdds[i][o] <= 0){return null;}
-            }
-
-            // Mutate speed
-            newSpeed[i] = this.geneMut(newSpeed[i], maxMutPct);
-            // Check if newSpeedSize violates constraints
-            if (newSpeed[i] < 0){return null;}
-
-            // Mutate signalNum
-            newSignalNum[i] = this.geneMut(newSignalNum[i], maxMutPct);
-            // Check if newSignalNum violates constraints
-            if (newSignalNum[i] < 0){return null;}
-            // Adjust other values based on newSignalNum
-            if (Math.floor(newSignalNum[i]) > Math.floor(this.signalNum[i]))
-            {
-                newMoveWeights[i] = math.concat(newMoveWeights[i], math.zeros(newMoveWeights[i].size()[0], 1));
-            }
-            else if (Math.floor(newSignalNum[i]) < Math.floor(this.signalNum[i]))
-            {
-                let ri = new Array(newMoveWeights[i].size()[0]);
-                let ci = new Array(newMoveWeights[i].size()[1] - 1);
-                for (let r = 0; r < ri.length; r++) {ri[r] = r;}
-                for (let c = 0; c < ci.length; c++) {ci[c] = c;}
-                newMoveWeights[i] = newMoveWeights[i].subset(math.index(ri, ci));
-            }
-
-            // TODO Mutate view
-
-            // TODO Mutate moveWeights
-            
-        }
-        return new Genome(newSexNum, newSexMin, newSexMax, newSexOdds, 
-                          newMaxSize, newSexSize, newBabySize, newSignalNum, 
-                          newEatOdds, newSpeed, newView, newMoveWeights);
+        let new_max_size = this.pct_mut(this.max_size, max_mut_pct);
+        if (new_max_size <= 0) {return null;}
+        let new_baby_frac = this.pct_mut(this.baby_frac, max_mut_pct);
+        if ((new_baby_frac <= 0) || (new_baby_frac >= 1)) {return null;}
+        let new_speed = this.pct_mut(this.speed, max_mut_pct);
+        if (new_speed <= 0) {return null;}
+        let new_ally_min = this.pct_mut(this.ally_min, max_mut_pct);
+        let new_ally_max = this.pct_mut(this.ally_max, max_mut_pct);
+        if (new_ally_min >= new_ally_max) {return null;}
+        let new_eat_ratio = Math.min(Math.max(this.fix_mut(this.eat_ratio, max_mut_pct), 0), 1);
+        let new_weights = ndarray(new Float64Array(this.weights.size), this.weights.shape);
+        let rand = ndarray(new Float64Array(this.weights.size), this.weights.shape);
+        ops.assign(new_weights, this.weights);
+        ops.random(rand);
+        ops.subseq(rand, 0.5);
+        ops.mulseq(rand, 2 * max_mut_pct);
+        ops.addeq(new_weights, rand);
+        return new Genome(new_ally_min, new_ally_max, new_max_size, new_baby_frac, new_eat_ratio, new_speed, new_weights);
     }
 
-    geneMut(gene, max)
+    pct_mut(gene, max)
     {
-        let randNorm = (Math.random() + Math.random() - Math.random() - Math.random()) / 2;
-        return gene + randNorm * gene * max;
+        let rand = Math.random() * 2 - 1;
+        return gene + rand * gene * max;
+    }
+
+    fix_mut(gene, max)
+    {
+        let rand = Math.random() * 2 - 1;
+        return gene + rand * max;
     }
 }
