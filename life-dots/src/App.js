5941e72ec22d63b96d988b04f8f0d57afa64846f
Ethan Prihar <ethanprihar@gmail.com>
2020-10-11 10:07:21 -0400

    started on dots, tested matrix multiplication speed

--- life-dots/src/App.js
+++ life-dots/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Genome from './models/Genome';
 
-//import Genome from './models/Genome.js';
 //import Dot from './models/Dot.js';
 
 var math = require("mathjs");
 var ndarray = require("ndarray");
 var ops = require("ndarray-ops");
+var gemm = require("ndarray-gemm");
+
+const view = 5
 
 function App() {
   console.log('Genome Test')
@@ -18,11 +20,48 @@ function App() {
   let baby_frac = 0.8;
   let eat_ratio = 0.5;
   let speed = 4.2;
-  let weights = ndarray(new Float64Array(450), [45,10]);
+  let weights = ndarray(new Float64Array((view * 2 + 1) * (view * 2 + 1) * 5 * 10), [(view * 2 + 1) * (view * 2 + 1) * 5, 10]);
   let g1 = new Genome(ally_min, ally_max, max_size, baby_frac, eat_ratio, speed, weights);
   let g2 = g1.mutate(0.1);
   console.log(g1);
   console.log(g2);
+  
+  console.log('Matrix Multiplication Test')
+  let a = ndarray(new Float64Array((view * 2 + 1) * (view * 2 + 1) * 5), [1,(view * 2 + 1) * (view * 2 + 1) * 5]);
+  ops.random(a)
+  let c = ndarray(new Float64Array(10), [1,10]);
+  let t = window.performance.now();
+  for (let i = 0; i < 1000; i++)
+  {
+    gemm(c, a, g2.weights);
+  }
+  console.log(window.performance.now() - t);
+  
+  console.log('Test Getting View')
+  let row = 3;
+  let col = 4;
+  let test1 = [];
+  for (let r = row-1; r < row + 2; r++)
+  {
+    for (let c = col - 1; c <= col + 1; c++)
+    {
+      test1.push(g2.weights.get(r, c));
+    }
+  }
+  console.log(test1);
+  let x = g2.weights.hi(row + 2, col + 2).lo(row - 1, col - 1);
+  let test2 = [];
+  for (let r = 0; r < x.shape[0]; r++)
+  {
+    for (let c = 0; c < x.shape[1]; c++)
+    {
+      test2.push(x.get(r, c))
+    }
+  }
+  console.log(test2)
+  console.log(x.data)
+
+
   return (
     <div className="App">
       <header className="App-header">
--- life-dots/src/models/Dot.js
+++ life-dots/src/models/Dot.js
@@ -1,52 +1,28 @@
 const math = require('mathjs');
 
-const SATURATION = 100;
-
 export default class Dot
 {
     constructor(mutable, genome, color=null)
     {
         this.mutable = mutable;
         this.genome = genome;
-        this.sex = this.initSex(genome.sexOdds);
-        this.size = this.initSex(genome.maxSize[this.sex], genome.babySize[this.sex]);
-        this.ticksUntilMove = genome.speed[this.sex];
-        this.signals = new Array(genome.signalNum[this.sex]).fill(0);
+        this.size = this.initSize(genome.max_size, genome.baby_frac);
+        this.ticks_until_move = genome.speed;
+        this.signal = 0
         this.color = color == null ? this.initColor() : color;
     }
 
-    initSex(sexOdds)
+    initSize(max_size, baby_frac)
     {
-        let prodSexOdds = new Array(Math.floor(this.genome.sexNum)).fill(0);
-        
-        for (let odds of sexOdds)
-        {
-            prodSexOdds = math.dotMultiply(prodSexOdds, odds);
-        }
-        var rand = Math.random() * math.sum(prodSexOdds);
-        var min = 0;
-        var max = sexOdds[0];
-        for (var i = 0; i < sexOdds.length; i++)
-        {
-            if (rand >= min && rand < max)
-            {
-                return i;
-            }
-            min += sexOdds[i];
-            max += sexOdds[i+1];
-        }
-    }
-
-    initSize(maxSize, babySize)
-    {
-        var minSize = maxSize * babySize;
-        return Math.random() * (maxSize - minSize) + minSize;
+        let min_size = max_size * baby_frac;
+        return Math.random() * (max_size - minSize) + min_size;
     }
 
     initColor()
     {
-        var color = []
-        for (var i = 0; i < 3; i++)
+        const SATURATION = 100;
+        let color = []
+        for (let i = 0; i < 3; i++)
         {
             color.push(Math.floor(Math.random() * (256-SATURATION)) + SATURATION);
         }
@@ -55,16 +31,16 @@ export default class Dot
 
     move(row, col, grids)
     {
-        //var views = grids.map(x => this.getViews(row, col, this.genome.perception, x))
+        //let views = grids.map(x => this.getViews(row, col, this.genome.perception, x))
         //this.signals = output.slice(9).map(x => x > 0);
-        //var choice = output.slice(0, 9).map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
-        //var newRow = (row - 1) + Math.floor(choice / 3);
-        //var newCol = (col - 1) + (choice % 3);
+        //let choice = output.slice(0, 9).map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+        //let newRow = (row - 1) + Math.floor(choice / 3);
+        //let newCol = (col - 1) + (choice % 3);
         //return [newRow, newCol];
         
     }
 
-    getView(row, col, pad, grid)
+    split()
     {
 
     }
